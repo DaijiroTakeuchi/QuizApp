039fc9c2269efeb8f7b1cd6f5ab8354a7b436bf5
Daijiro Takeuchi <takeuchi@tam-bourine.co.jp>
2020-01-24 16:22:21 +0900

    mod インスタンス呼び出し修正

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,6 @@
-// const api = new API;
-const quiz = new Quiz;
-
 const getQuestion = document.getElementById('get-questions');
 
 getQuestion.addEventListener('click', (e) => {
   e.preventDefault();
-  quiz.getQuiz();
+  getQuestions();
 });
